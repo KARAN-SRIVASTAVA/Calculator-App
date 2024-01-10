0a9d11a3745b3634b6355c18e8ecb0e0671b16a2
KARAN-SRIVASTAVA <srivastavakaran319@gmail.com>
2024-01-10 15:25:38 +0530

    change style.css(Shadow added) and Js File (Optimizations added)

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
 const resultDisplay = document.querySelector("[resultDisplay]");
 const inputDisplay = document.querySelector("[inputDisplay]");
 const allButtons = document.querySelectorAll(".btn");
-const validKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const validOperator = ["(", ")", "%", "/", "*", "-", "+"];
+const validCharacters = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "(", ")", "%", "/", "*", "-", "+"];
+
 let inputResult = "";
 let outputResult = "0";
-showOutputResult();
 let cursorPosition = 0;
 
+// Display the initial output result
+showOutputResult();
+
+// Set focus on the input display when the window is clicked
 window.addEventListener("click", setFocus);
 
+// Clear the input screen
 function clearInputScreen() {
   inputResult = "";
   showInputResult();
 }
 
+// Display the input result in the input display
 function showInputResult() {
   inputDisplay.value = inputResult;
   setFocus();
   inputDisplay.setSelectionRange(cursorPosition, cursorPosition);
 }
 
+// Display the output result in the result display
 function showOutputResult() {
   resultDisplay.innerHTML = outputResult;
 }
 
+// Collect all input from the input display
 function collectAllInput() {
   inputResult = inputDisplay.value;
 }
 
+// Check the result for special cases
 function checkResult(data) {
   if (data == "Infinity") {
     outputResult = "Cannot divide by zero";
   }
 }
 
+// Set focus on the input display
 function setFocus() {
   inputDisplay.focus();
 }
 
+// Evaluate the input result
 function evaluateResult() {
   let newResult = "0";
   try {
@@ -52,25 +62,26 @@ function evaluateResult() {
   showOutputResult();
 }
 
+// Update the cursor position when the input display is clicked
 inputDisplay.addEventListener("click", () => {
   cursorPosition = inputDisplay.selectionStart;
-  console.log(cursorPosition);
 });
 
+// Add a value to the input result at the current cursor position
 function addValue(data) {
-  inputDisplay.setSelectionRange(cursorPosition, cursorPosition);
   inputResult =
     inputResult.substring(0, cursorPosition) +
     data +
     inputResult.substring(cursorPosition);
   cursorPosition += 1;
+  // () is only special key which one have two characters
   if (data === "()") cursorPosition += 1;
-  console.log(cursorPosition);
-  console.log(cursorPosition);
   showInputResult();
 }
 
+// Remove the character at the current cursor position
 function removeCharacter() {
+  // handling the case when the cursor is located at initial position and request comes to delete a character
   if (cursorPosition === 0) return;
   inputDisplay.setSelectionRange(cursorPosition, cursorPosition);
   inputResult =
@@ -80,6 +91,7 @@ function removeCharacter() {
   showInputResult();
 }
 
+// Handle the button click events
 function handleButtonClick(buttonValue) {
   if (buttonValue === "AC") {
     clearInputScreen();
@@ -91,21 +103,22 @@ function handleButtonClick(buttonValue) {
   } else {
     addValue(buttonValue);
   }
-  console.log(inputResult);
 }
 
+// Attach click event listeners to all the buttons
 allButtons.forEach((button) => {
   button.addEventListener("click", () => {
     handleButtonClick(button.value);
   });
 });
 
+// Handle keydown events on the input display
 inputDisplay.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+    // handling edge cases
     if (e.key == "ArrowLeft" && cursorPosition > 0) cursorPosition -= 1;
     if (e.key == "ArrowRight" && cursorPosition < inputResult.length)
       cursorPosition += 1;
-    console.log(cursorPosition);
   } else if (e.key === "Backspace") {
     removeCharacter();
     setTimeout(() => {
@@ -113,15 +126,9 @@ inputDisplay.addEventListener("keydown", (e) => {
     }, 0);
   } else if (e.key == "Enter") {
     evaluateResult();
-  } else if (validKeys.indexOf(e.key) !== -1) {
-    addValue(validKeys[validKeys.indexOf(e.key)]);
-    setTimeout(() => {
-      showInputResult();
-    }, 0);
-  } else {
-    if (validOperator.indexOf(e.key) !== -1) {
-      addValue(validOperator[validOperator.indexOf(e.key)]);
-    }
+  } else if (validCharacters.indexOf(e.key) !== -1) {
+    addValue(validCharacters[validCharacters.indexOf(e.key)]);
+    // setTimeout is used to show results after adding a value to the input
     setTimeout(() => {
       showInputResult();
     }, 0);
